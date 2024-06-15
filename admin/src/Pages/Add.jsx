5ae4fc39774dbcbc164ecdb1d3cb98c201af6d9e
Harsh <126787857+Harshp2405@users.noreply.github.com>
2024-06-15 08:15:24 +0530

    Commit

--- admin/src/Pages/Add.jsx
+++ admin/src/Pages/Add.jsx
@@ -7,16 +7,16 @@ export const Add = () => {
   const urladd = "http://localhost:2024"
   const [image, setimage] = useState(false)
   const [data, setdata] = useState({
-    Foodname:"",
-    Fooddescription:"",
-    price:"",
-    category:"Salad",
+    Foodname: "",
+    Fooddescription: "",
+    price: "",
+    category: "Salad",
   })
 
-  const changeHandle = (e)=>{
+  const changeHandle = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setdata(data=>({...data,[name]:value}))
+    setdata(data => ({ ...data, [name]: value }))
   }
   useEffect(() => {
     console.log(data)
@@ -25,32 +25,32 @@ export const Add = () => {
   const onsubmit = async (e) => {
     e.preventDefault();
     const formdata = new FormData();
-    formdata.append("Foodname",data.Foodname)
-    formdata.append("Fooddescription",data.Fooddescription)
-    formdata.append("price",Number(data.price))
-    formdata.append("category",data.category)
-    formdata.append("image",image)
+    formdata.append("Foodname", data.Foodname)
+    formdata.append("Fooddescription", data.Fooddescription)
+    formdata.append("price", Number(data.price))
+    formdata.append("category", data.category)
+    formdata.append("image", image)
 
-    const response = await axios.post(`${urladd}/api/food/add`,formdata);
+    const response = await axios.post(`${urladd}/api/food/add`, formdata);
     if (response.data.success) {
       setdata({
-        Foodname:"",
-        Fooddescription:"",
-        price:"",
-        category:"Salad",
+        Foodname: "",
+        Fooddescription: "",
+        price: "",
+        category: "Salad",
       })
       setimage(false)
     }
-    else{
+    else {
       console.log("Error")
       console.log(e)
-      setdata({
-        Foodname:"",
-        Fooddescription:"",
-        price:"",
-        category:"Salad",
-      })
-      setimage(false)
+      // setdata({
+      //   Foodname: "",
+      //   Fooddescription: "",
+      //   price: "",
+      //   category: "Salad",
+      // })
+      // setimage(false)
     }
   }
 
@@ -61,22 +61,22 @@ export const Add = () => {
           <div className="imgupload flexcol">
             <p>upload img</p>
             <label htmlFor="image">
-              <img className='upimg' required src={image?URL.createObjectURL(image):assetsa.upload_area} alt="" />
+              <img className='upimg' required src={image ? URL.createObjectURL(image) : assetsa.upload_area} alt="" />
             </label>
-            <input onChange={(e)=>setimage(e.target.files[0])} type="file" id='image' name='image' hidden required />
+            <input onChange={(e) => setimage(e.target.files[0])} type="file" id='image' name='image' hidden required />
           </div>
           <div className="addname flexcol">
             <p>Product Name</p>
-            <input onChange={changeHandle} value={data.Foodname} type="text" name='Foodname' placeholder='Name' required/>
+            <input onChange={changeHandle} value={data.Foodname} type="text" name='Foodname' placeholder='Name' required />
           </div>
           <div className="adddesc flexcol">
             <p>Product Desc</p>
             <textarea onChange={changeHandle} value={data.Fooddescription} rows='6' name='Fooddescription' placeholder='Food description' required />
           </div>
           <div className="addcatprice flexcol">
-              <p>Categeory</p>
+            <p>Categeory</p>
             <div className="cate flexcol">
-              <select onChange={changeHandle}  name="category" >
+              <select onChange={changeHandle} name="category" >
                 <option value="Salad">Salad</option>
                 <option value="Rolls">Rolls</option>
                 <option value="Desert">Desert</option>
